be1ed4d0a2f3bab995ebffde6244ee7a5e7d81a5
gyaanmilan <gyaanmilan13@gmail.com>
2018-01-08 23:01:46 +0530

    Initial CheckIn

---
+++ MvcApplicationWithKnockout/Controllers/SampleController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplicationWithKnockout.Controllers
+{
+    public class SampleController : Controller
+    {
+        //
+        // GET: /Sample/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Save(string name,string email,string address)
+        {
+            bool isSave = false;
+            if (!string.IsNullOrEmpty(name)&& !string.IsNullOrEmpty(address))
+            {
+                isSave = true;
+            }
+            return Json(isSave, JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
