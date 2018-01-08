79ac75d7857147b517a926ebe2e4095de66ed706
gyaanmilan <gyaanmilan13@gmail.com>
2018-01-08 23:15:19 +0530

    https://github.com/GyaanMilan/Notify.git

---
+++ MvcApplicationWithKnockout/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplicationWithKnockout.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
